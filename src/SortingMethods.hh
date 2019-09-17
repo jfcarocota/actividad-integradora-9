c2dadde48e6652f6577101c9af3a24b4c6402ce3
jfcarocota <jfcarocota@gmail.com>
2019-09-16 23:37:45 -0700

    heap sort solved

--- src/SortingMethods.hh
+++ src/SortingMethods.hh
@@ -3,11 +3,13 @@ class SortingMethods
 private:
     /* data */
     static void Swap(int*, int*); 
+    static void Heapify(int[], int, int);
 public:
     SortingMethods(/* args */);
     ~SortingMethods();
     static const void ShellSort(int[], int);
     static void SelectionSort(int[], int); 
+    static void HeapSort(int[], int);
 };
 
 SortingMethods::SortingMethods(/* args */)
@@ -59,3 +61,38 @@ void SortingMethods::SelectionSort(int arr[], int n)
         SortingMethods::Swap(&arr[min_idx], &arr[i]);  
     }  
 }  
+
+void SortingMethods::Heapify(int arr[], int n, int i) 
+{ 
+    int largest = i; 
+    int l = 2*i + 1; 
+    int r = 2*i + 2; 
+  
+    if (l < n && arr[l] > arr[largest])
+    {
+        largest = l;
+    }  
+    if (r < n && arr[r] > arr[largest])
+    {
+        largest = r; 
+    } 
+    if (largest != i) 
+    { 
+        std::swap(arr[i], arr[largest]); 
+        SortingMethods::Heapify(arr, n, largest); 
+    } 
+} 
+  
+void SortingMethods::HeapSort(int arr[], int n) 
+{ 
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        SortingMethods::Heapify(arr, n, i); 
+    } 
+  
+    for (int i=n-1; i>=0; i--) 
+    { 
+        std::swap(arr[0], arr[i]); 
+        SortingMethods::Heapify(arr, i, 0); 
+    } 
+} 
--- src/main.cc
+++ src/main.cc
@@ -5,15 +5,21 @@ int main()
 {
     int arr[] = {5, 8, 1, 9, 4, 7, 3, 2, 6};
     int size = sizeof(arr)/sizeof(arr[0]); 
-    std::cout << "Sorting methods" << std::endl;
+    /*std::cout << "Sorting methods" << std::endl;
     SortingMethods::ShellSort(arr, size);
     std::cout << "\nShell Sort:" << std::endl;
     for(int i = 0; i < size; i++)
     {
         std::cout << arr[i] << " ";
-    }
-    std::cout << "\nSelection Sort:" << std::endl;
+    }*/
+    /*std::cout << "\nSelection Sort:" << std::endl;
     SortingMethods::SelectionSort(arr, size);
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }*/
+    std::cout << "\nHeap Sort:" << std::endl;
+    SortingMethods::HeapSort(arr, size);
     for(int i = 0; i < size; i++)
     {
         std::cout << arr[i] << " ";
