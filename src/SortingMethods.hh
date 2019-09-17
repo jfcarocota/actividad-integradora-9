4df71d45cf63fbb005cda6fb935f5a192848c0e1
jfcarocota <jfcarocota@gmail.com>
2019-09-16 22:59:01 -0700

    selection sort solved

--- src/SortingMethods.hh
+++ src/SortingMethods.hh
@@ -2,10 +2,12 @@ class SortingMethods
 {
 private:
     /* data */
+    static void Swap(int*, int*); 
 public:
     SortingMethods(/* args */);
     ~SortingMethods();
     static const void ShellSort(int[], int);
+    static void SelectionSort(int[], int); 
 };
 
 SortingMethods::SortingMethods(/* args */)
@@ -32,3 +34,28 @@ const void SortingMethods::ShellSort(int arr[], int n)
         } 
     } 
 } 
+
+void SortingMethods::Swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void SortingMethods::SelectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+  
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+        // Swap the found minimum element with the first element  
+        SortingMethods::Swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
--- src/main.cc
+++ src/main.cc
@@ -5,11 +5,18 @@ int main()
 {
     int arr[] = {5, 8, 1, 9, 4, 7, 3, 2, 6};
     int size = sizeof(arr)/sizeof(arr[0]); 
-    std::cout << "Hello world" << std::endl;
+    std::cout << "Sorting methods" << std::endl;
     SortingMethods::ShellSort(arr, size);
+    std::cout << "\nShell Sort:" << std::endl;
     for(int i = 0; i < size; i++)
     {
-        std::cout << arr[i];
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\nSelection Sort:" << std::endl;
+    SortingMethods::SelectionSort(arr, size);
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
     }
     std::getchar();
     return 0;
